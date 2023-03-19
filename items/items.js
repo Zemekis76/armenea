695874bfcb7a9f92420022cfe50fb9529f035c79
Zemekis76 <47531155+Zemekis76@users.noreply.github.com>
2023-03-19 12:43:16 +0100

    Create items.js

---
+++ items/items.js
@@ -0,0 +1,12 @@
+// items.js
+import { ArmeneaItemSheet, ArmeneaWeaponSheet } from "./sheets.js";
+
+class ArmeneaItem extends Item {
+  // ...
+}
+
+class ArmeneaWeapon extends ArmeneaItem {
+  // ...
+}
+
+// Rest des Codes...
