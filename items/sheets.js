caec2ffc2984ab50d2563e67427cdc43e11c855f
Zemekis76 <47531155+Zemekis76@users.noreply.github.com>
2023-03-19 12:44:08 +0100

    Create sheets.js

---
+++ items/sheets.js
@@ -0,0 +1,10 @@
+// sheets.js
+class ArmeneaItemSheet extends ItemSheet {
+  // ...
+}
+
+class ArmeneaWeaponSheet extends ArmeneaItemSheet {
+  // ...
+}
+
+export { ArmeneaItemSheet, ArmeneaWeaponSheet };
